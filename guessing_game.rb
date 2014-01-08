cf4b8855c6219cf478cc5c72a0d42e3ffbbcb1ae
mbetts7 <mbetts7@gmail.com>
2014-01-07 22:57:29 -0800

    tracking lab day 1

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,11 +1,11 @@
-num = rand(100) # define a number randomly between 0 and 100
+num = rand(101) # define a number randomly between 0 and 100 #ALT rand(1..100)
 
 guess_count = 1 # start guess counter at 1 so when it gets guessed that is the 1 and if it's not you increment by 1
 
 puts "Guess a number between 1 and 100." # prompt user for input
-guess = gets.to_i # get guess as an integer
+guess = gets.to_i # get guess and convert it to an integer
 
-until guess == num # begin loop until this expression is true
+until guess == num # begin loop until we get the right answer #ALT, could use while instead of until, but until is more preferred
 
 	if guess > num # begin control flow -- if guess is higher than number than instruct them to guess lower
 		puts "The number is lower than #{guess}.  Guess again." 
@@ -18,8 +18,4 @@ until guess == num # begin loop until this expression is true
 	end
 end
 
-
 puts "#{guess} is the correct number! You got it in #{guess_count} tries."
-
-
-
--- print_with_frame.rb
+++ print_with_frame.rb
@@ -1,6 +1,6 @@
 print "Please enter a phrase to be framed: " #prompt user
 phrase = gets.chomp.split # get phrase and save as array
-wlength = phrase[0].length # defining wlength as 0 then incrementing to max word length below
+wlength = phrase[0].length # defining wlength as 0 length to start
 
 phrase.each do |word| # start looping of block
 	if word.length > wlength  # iterate through phrase array that user inputs.  If length is greater than previous, then save, if not, then end
@@ -9,9 +9,9 @@ phrase.each do |word| # start looping of block
 	end
 end
 
-framelength = wlength + 4
+framelength = wlength + 4 #define width of frame, adding 4 characters for space
 
-# make it centered in the frame
+# make output it centered in the frame
 puts "*" * framelength
 
 phrase.each do |word|
@@ -28,3 +28,11 @@ phrase.each do |word|
 end
 
 puts "*" * framelength
+
+
+# The story setup
+# User input string
+# split string
+# iterate over array
+# find longest word
+# need to print
